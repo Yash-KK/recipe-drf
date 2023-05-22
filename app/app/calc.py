adb88a9bb1ea75c48f26f5829bd8b492b9a7e146
Yash-KK <reachyashk@gmail.com>
2023-05-22 07:46:03 +0530

    configured docker compose and checks.yml

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,13 @@
-"Calculate"
+"""
+Calculator functions
+"""
 
-def add(x,y):
+
+def add(x, y):
+    """Add x and y and return result."""
     return x + y
 
-def subtract(x,y):
-    return y-x
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,14 +1,22 @@
+"""
+Sample tests
+"""
 from django.test import SimpleTestCase
 
 from .calc import add, subtract
 
 
-class CalcTests(SimpleTestCase):    
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
     def test_add_numbers(self):
-        res = add(5,6)
-        self.assertEqual(res,11)
+        """Test adding numbers together."""
+        res = add(5, 6)
+
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = subtract(10,15)
-        self.assertEqual(res,5)
-        
+        """Test subtracting numbers."""
+        res = subtract(10, 15)
+
+        self.assertEqual(res, 5)
